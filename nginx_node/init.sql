47bce8809cbcfa08f79599ba20a254955d299de5
Fabrício J. Silva <fabriciosilva@rvdigitalbrasil.com.br>
2024-02-15 15:12:19 -0300

    Retira o volume do node

--- nginx_node/init.sql
+++ nginx_node/init.sql
@@ -1,5 +1,3 @@
 CREATE DATABASE IF NOT EXISTS nodedb;
--- USE dev;
--- CREATE TABLE IF NOT EXISTS (...);
 
 create table if not exists people(id int not null auto_increment, name varchar(255), primary key (id));
